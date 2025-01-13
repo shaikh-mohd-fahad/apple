42ba4124507e8473bd6b2ddef3cc80c48286ed10
shaikh-mohd-fahad <fahadazmilko@gmail.com>
2025-01-14 01:35:40 +0530

    Final setup ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,11 @@
 import React from 'react'
-import { gsap } from "gsap";
-import { useGSAP } from "@gsap/react";
 function App() {
-  useGSAP(()=>{
-    gsap.to("#div1",{
-      x:250,
-      duration:3,
-      ease:"back.inOut",
-      rotate:360
-    })
-  })
+
   return (
     <>
-      <div className='w-20 h-20 bg-blue-400 m-5 radius-lg' id="div1"> 
+      <main className='bg-black'>
 
-      </div>
+      </main>
     </>
   )
 }
