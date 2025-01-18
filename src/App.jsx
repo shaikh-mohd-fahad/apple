472edf76ec4f73db909e2b29ffb8f6dbe2f4b1de
shaikh-mohd-fahad <fahadazmilko@gmail.com>
2025-01-18 22:17:37 +0530

    hero section completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import Hero from './components/Hero'
+import Navbar from './components/Navbar'
+import Highlights from './components/Highlights'
 function App() {
 
   return (
     <>
       <main className='bg-black'>
-
+        <Navbar/>
+        <Hero/>
+        <Highlights/>
       </main>
     </>
   )
